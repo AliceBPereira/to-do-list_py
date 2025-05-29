bd14e96426f3df8b70eb4c6f7b42aac022fd3bb3
Alice Barros Pereira <103052000+AliceBPereira@users.noreply.github.com>
2025-05-29 08:50:41 -0300

    feat: create gerenciador.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from tarefa import Tarefa
+from persistencia import carregar_tarefas, salvar_tarefas
 
 tarefa1 = Tarefa("Estudar Python", "Estudar a aula 2 de Python", "10/10/2025")
-print(tarefa1)  # Exibe a representação da tarefa
-print(tarefa1.detalhes())
+
+
+tarefa2 = Tarefa(titulo="Fazer compras", descricao="Comprar leite e pão", data_vencimento= "15/10/2024", status="Pendente")
+
+salvar_tarefas([tarefa1, tarefa2], "tarefas.pkl")  # Salva as tarefas em um arquivo
+tarefas = carregar_tarefas("tarefas.pkl")  # Carrega as tarefas do arquivo
+
+print(tarefas[0].detalhes())  # Exibe os detalhes da primeira tarefa
--- tarefa.py
+++ tarefa.py
@@ -11,7 +11,7 @@ class Tarefa:
     # Diferença de string e representation. string é o que aparece quando chamamos a função str() e representation é o que aparece quando chamamos a função repr()
     def __repr__(self ):
         return f"Tarefa(titulo='(self.titulo)', descricao='(self.descricao)', " \
-               f"data_vencimento='(self.data_vencimento)', status='(self.status)')"
+               f"data_vencimento='(self.data_vencimento.strftime('%d/%m/%Y'))', status='(self.status)')"
     
     def marcar_como_concluida(self):
         # Marca a tarefa como concluída
@@ -20,6 +20,20 @@ class Tarefa:
     def esta_atrasada(self):
         # Verifica se a data de vencimento é menor que a data atual
         return datetime.now() > self.data_vencimento and self.status == 'Pendente'
+    
+    def editar_titulo(self, titulo):
+        # Edita o título da tarefa
+        self.titulo = titulo
+
+    def editar_descricao(self, descricao):
+        # Edita a descrição da tarefa
+        self.descricao = descricao
+
+    def editar_data_vencimento(self, data_vencimento):
+        # Edita a data de vencimento da tarefa
+        self.data_vencimento = datetime.strptime(data_vencimento, "%d/%m/%Y")
+
+
     def detalhes(self):
         status = "Atrasada" if self.esta_atrasada() else self.status
         return (f"Título: {self.titulo}\n"
